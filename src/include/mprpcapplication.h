e0c26b2a16d4403092f3c44b84cc29efc71925bd
催码讷！ <96453850+adfdwf@users.noreply.github.com>
2025-12-01 12:52:52 +0800

    Update mprpcapplication.h

--- src/include/mprpcapplication.h
+++ src/include/mprpcapplication.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "mprpcconfig.h"
-// mprpc框架的基础类，负责框架的一些初始化操作
+// mprpc框架的基础类，负责框架的一些初始化操作，如通过命令行指定配置文件，利用单例模式全局管理配置
 class MpRpcApplication{
     public:
         static void Init(int argc, char **argv);
